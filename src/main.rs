61690b8a93b37fb298a3fa74f8e484b28a215632
Robin Mattheussen <robin.mattheussen@gmail.com>
2020-10-18 23:12:40 +0200

    Run actix_rt inside an existing tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web, Responder};
+use tokio::prelude::*;
+
+async fn hello_world() -> impl Responder {
+    "Hello World!"
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let local = tokio::task::LocalSet::new();
+    let sys = actix_rt::System::run_in_tokio("server", &local);
+    let server_res = HttpServer::new(|| App::new().route("/", web::get().to(hello_world)))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await?;
+    sys.await?;
+    Ok(server_res)
 }
